fa3516ffe798e6929b2ef11c9c0340c8d0129aa6
Lois <jemimahsalazar@gmail.com>
2026-02-21 22:01:29 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,7 +1,15 @@
-/* Menu Hamburger */
-const navBtn = document.querySelector(".menu-btn");
+/*Menu Hamburger */
+const menuBtn = document.querySelector(".menu-btn");
 const navList = document.querySelector(".nav-list");
 
-navBtn.addEventListener("click", function () {
+
+menuBtn.addEventListener("click", () => {
   navList.classList.toggle("active");
+});
+
+
+document.querySelectorAll(".nav-list a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navList.classList.remove("active");
+  });
 });
